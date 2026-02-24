1d79708a07a6e48b2293ea7837608a56487fb4dc
MugoBrian <mikanmuchiri@gmail.com>
2026-02-24 19:06:18 +0300

    Improved the hero  and membership stats cards section's layout and responsiveness.

--- script.js
+++ script.js
@@ -5,21 +5,25 @@ const members = [
     name: "Benard Makodingo",
     score: 350,
     avatar: "https://randomuser.me/api/portraits/men/69.jpg",
+    color: "#e2bbe2",
   },
   {
     name: "kigSmall Foot",
     score: 225,
     avatar: "https://randomuser.me/api/portraits/men/84.jpg",
+    color: "#aef0d2",
   },
   {
     name: "Caroline Likhol",
     score: 117,
     avatar: "https://randomuser.me/api/portraits/women/69.jpg",
+    color: "#f1d9c0",
   },
   {
     name: "Caroline Likhol",
     score: 321,
     avatar: "https://randomuser.me/api/portraits/women/69.jpg",
+    color: "#ebcaeb",
   },
 ];
 
@@ -32,10 +36,12 @@ function renderMembers() {
   grid.innerHTML = members
     .map(
       (member) => `
-    <div class="col-6">
-      <div class="member-card d-flex flex-column align-items-center p-3">
-        <img src="${member.avatar}" alt="${member.name}" class="member-avatar mb-2"/>
+    <div class="col-6 col-md-3">
+      <div class="member-card d-flex flex-column align-items-center p-3" style="background-color: ${member.color};">
+      <div class="member-header">
+      <img src="${member.avatar}" alt="${member.name}" class="member-avatar mb-2"/>
         <span class="member-name">${member.name}</span>
+      </div>  
         <span class="member-score">${member.score}</span>
       </div>
     </div>
